4f0eabb55b5c14e7efe695b382538ce6634dcfd2
davim-o <davi.miranda@escolar.ifrn.edu.br>
2026-04-17 11:26:21 -0300

    alguma coisa ai

--- Loja/loja/admin.py
+++ Loja/loja/admin.py
@@ -4,3 +4,20 @@ from .models import *
 admin.site.register(Fabricante)
 admin.site.register(Categoria)
 admin.site.register(Produto)
+
+class FabricanteAdmin(admin.ModelAdmin):
+# Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao',
+'preco', 'categoria',)
+    empty_value_display = 'Vazio'
+
+
+
+
+admin.site.register(Fabricante,FabricanteAdmin)
+admin.site.register(Categoria)
+admin.site.register(Produto, ProdutoAdmin)
